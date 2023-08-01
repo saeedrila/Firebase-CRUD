391d65cc15d4a0a448d14848bcc1bbcead955525
saeedrila <saeedrila@gmail.com>
2023-08-01 19:05:26 +0530

    New user with email has been created

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ function App() {
   return (
     <div className="App">
       <button onClick={()=>{
-        Firebase.firestore().collection('products').get().then((snapshot)=>{
-          console.log(snapshot)
+        Firebase.auth().createUserWithEmailAndPassword('alice@gmail.com','1234567890')
+        .then((userCredential) => {
+          // Signed in 
+          var user = userCredential.user;
+          Firebase.firestore().collection('products').get().then((snapshot)=>{
+            console.log(snapshot)
+          })
+          // ...
         })
-        
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ..
+        });
+
         console.log('Clicked')
       }}>Click Me!</button>
     </div>
