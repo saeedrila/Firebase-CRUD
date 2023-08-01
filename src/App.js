56dcf675750c9912a499a5f18df0267b24be6d6d
saeedrila <saeedrila@gmail.com>
2023-08-01 18:17:10 +0530

    Update functionality

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ function App() {
   return (
     <div className="App">
       <button onClick={()=>{
-        Firebase.firestore().collection('products').add({
-          name:'Redmi',
-          price:15000,
-          type:'Smartphone'
+        Firebase.firestore().collection('products').doc('pv25wXIL81vYzMQb5Xgh').set({
+          name:'Updated'
         })
         console.log('Clicked')
       }}>Click Me!</button>
